2c612e80632c01f36e8d7b88827b28583d22bbaf
orenbenkiki <oren@ben-kiki.org>
2024-06-12 21:08:27 +0300

    Builds on new OS!

--- deps/add_pkgs.jl
+++ deps/add_pkgs.jl
@@ -1,4 +1,12 @@
 using Pkg
+using TOML
+
+project = TOML.parse(read("Project.toml", String))
+
+for pkg in keys(project["deps"])
+    println("Adding $(pkg):")
+    Pkg.add(pkg)
+end
 
 for pkg in (
     "Aqua",
